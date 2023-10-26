547add809c27726aa74433bfb4fefd13840d2285
Aijaz Mohammad <gnudoc.git@gmail.com>
2023-10-26 21:32:37 +0100

    A few more files

---
+++ ex23.py
@@ -0,0 +1,18 @@
+fruit = [
+    ['apples', 12, 'aaa'], ['oranges', 1, 'b'],
+    ['pears', 2, 'a'], ['grapes', 14,'ur']]
+
+cars = [
+    ['cadillac', ['black', 'big', 34500]],
+    ['corvette', ['red', 'little', 1000000]],
+    ['ford', ['blue', 'medium', 1234]],
+    ['bmw', ['white', 'baby', 7890]]
+    ]
+
+languages = [
+    ['python', ['slow', ['terrible', 'mush']]],
+    ['javascript', ['moderate', ['alright', 'bizarre']]],
+    ['perl6', ['moderate', ['fun', 'weird']]],
+    ['c', ['fast', ['annoying', 'dangerous']]],
+    ['forth', ['fast', ['fun', 'difficult']]]
+    ]
---
+++ ex24.py
@@ -0,0 +1,21 @@
+fruit = [
+    {'kind': 'apples', 'count': 12, 'rating': 'aaa'},
+    {'kind': 'oranges', 'count': 1, 'rating': 'b'},
+    {'kind': 'pears', 'count': 2, 'rating': 'a'},
+    {'kind': 'grapes', 'count': 14, 'rating': 'ur'}
+    ];
+
+cars = [
+    {'type': 'cadillac', 'color': 'black', 'size': 'big', 'miles': 34500},
+    {'type': 'corvette', 'color': 'red', 'size': 'little', 'miles': 1000000},
+    {'type': 'ford', 'color': 'blue', 'size': 'medium', 'miles': 1234},
+    {'type': 'bmw', 'color': 'white', 'size': 'baby', 'miles': 7890}
+    ];
+
+languages = [
+    {'name': 'python', 'speed': 'slow', 'opinion': ['terrible', 'mush']},
+    {'name': 'javascript', 'speed': 'moderate', 'opinion': ['alright', 'bizarre']},
+    {'name': 'perl6', 'speed': 'moderate', 'opinion': ['fun', 'weird']},
+    {'name': 'c', 'speed': 'fast', 'opinion': ['annoying', 'dangerous']},
+    {'name': 'forth', 'speed': 'fast', 'opinion': ['fun', 'difficult']}
+    ];
